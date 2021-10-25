4c3250bb0285f4aa4c5fd06c798355854db53d99
mayra_dominguez <mayra.dominguez@rappi.com>
2021-10-24 22:31:42 -0500

    mi primer commmit

---
+++ src/paquete1/atributosAndMetodos.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class atributosAndMetodos {
+    protected int valorUno;
+    protected int valordos;
+    protected String valortres;
+
+    protected  void metodovalor(){
+ System.out.println("xxx");
+    }
+}
